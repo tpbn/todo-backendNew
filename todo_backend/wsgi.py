29b23191d31c9c30e23cd9a98b2b5d98155b2fcc
tpbn <pabillarantrishamay2024@gmail.com>
2025-04-05 01:25:56 +0800

    Fix Procfile and wsgi.py references

--- todo_backend/wsgi.py
+++ todo_backend/wsgi.py
@@ -1,13 +1,5 @@
-"""
-WSGI config for todo_backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+
 
 from django.core.wsgi import get_wsgi_application
 
