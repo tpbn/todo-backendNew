3c932fc8b617c178a40ea701dae8a613b08612c1
tpbn <pabillarantrishamay2024@gmail.com>
2025-04-10 19:00:03 +0800

    fix secure superuser creation view

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
-from .views_superuser import create_superuser
+from .views_superuser import create_superuser  # <-- Make sure this name matches your function
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = router.urls + [
-    path('create-superuser/', create_superuser_view, name='create-superuser'),  # register
+    path('create-superuser/', create_superuser, name='create-superuser'),  # <- This must be exact
 ]
