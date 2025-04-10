3180f83becb56f5922de24739c76d6cbb64945af
tpbn <pabillarantrishamay2024@gmail.com>
2025-04-10 21:47:10 +0800

    Step 5 complete: secure view and token login

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
+from rest_framework.authtoken.views import obtain_auth_token  # added this line for tken
+from .views import TaskViewSet, SecureHelloView 
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('api-token-auth/', obtain_auth_token),
+    path('secure-hello/', SecureHelloView.as_view()),
+]
--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'todo',
-    'rest_framework'
-]
+    'rest_framework',
+    'rest_framework.authtoken',
+]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
