d0f0d5d5602c42b9436ec42c9443ebf4f0447032
tpbn <pabillarantrishamay2024@gmail.com>
2025-04-10 19:57:50 +0800

    remove superuser_view for security

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
-from .views_superuser import create_superuser  # <-- Make sure this name matches your function
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
-urlpatterns = router.urls + [
-    path('create-superuser/', create_superuser, name='create-superuser'),  # <- This must be exact
-]
+urlpatterns = router.urls
