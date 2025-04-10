2af694b33d7d69a5012cbb92b68328531754ec66
tpbn <pabillarantrishamay2024@gmail.com>
2025-04-10 18:31:58 +0800

    Fix: Register create-superuser route in urlpatterns

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
-from .views_superuser import create_superuser
-
+from .views_superuser import create_superuser_view
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 
-urlpatterns = router.urls
-path('create-superuser/', create_superuser),
+urlpatterns = router.urls + [
+    path('create-superuser/', create_superuser_view, name='create-superuser'),
+]
